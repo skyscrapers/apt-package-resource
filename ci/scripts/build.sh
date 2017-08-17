6feed53c21930a5d4fa10e16884a4488f390a393
Ringo De Smet <ringo@de-smet.name>
2017-08-17 18:16:56 +0200

    Fixing the paths.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -12,7 +12,7 @@ export GOPATH=$PWD
 
 # Now we must move our code from the current directory ./apt-package-resource to $GOPATH/src/github.com/skyscrapers/apt-package-resource
 mkdir -p src/github.com/skyscrapers/
-cp -R ./hello-go src/github.com/JeffDeCola/.
+cp -R ./apt-package-resource src/github.com/skyscrapers/.
 
 # All set and everything is in the right place for go
 echo "Gopath is: " $GOPATH
